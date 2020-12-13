d6e0e6fd1b7865ff68d5be321ff456105a464ad7
junqi <junqili259@gmail.com>
2020-12-12 23:52:27 -0500

    Initial Commit

---
+++ Anime-Alert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Anime-Alert
+//
+//  Created by Jun Qi Li on 12/12/20.
+//  Copyright © 2020 Jun Qi Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
