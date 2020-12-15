47175e04b86c3f86b870858dea8235a988b59c8e
junqili <junqili259@gmail.com>
2020-12-15 07:43:43 -0500

    Api (#5)
    * Delete settings.json
    
    * added .vscode to gitignore
    
    * Update python-app.yml
    
    * Add endpoint for any prior seasonal groups for anime
    
    * test endpoint any_Season
    
    * update
    
    * Update test_main.py

--- api/test_main.py
+++ api/test_main.py
@@ -7,3 +7,8 @@ def test_current_Season():
     response = client.get("/")
     assert response.status_code == 200
 
+
+def test_any_Season():
+    response = client.get("/season?season=SUMMER&seasonYear=2020")
+    assert response.status_code == 200
+    
