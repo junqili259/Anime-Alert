9d11212a22cfb8313739e087df94446b7ce7eaef
junqili <junqili259@gmail.com>
2020-12-17 13:35:54 -0500

    Networking to api (#7)
    * created urlsession to api
    
    * update

---
+++ Anime-Alert/Network.swift
@@ -0,0 +1,58 @@
+//
+//  Network.swift
+//  Anime-Alert
+//
+//  Created by Jun Qi Li on 12/16/20.
+//  Copyright © 2020 Jun Qi Li. All rights reserved.
+//
+
+import Foundation
+
+
+protocol AnimeModelProtocol {
+    func animeRetrieved(_ anime: AnimePage)
+}
+
+class Network {
+    
+    var delegate: AnimeModelProtocol?
+
+    func getSeasonalAnime() {
+        
+        // Temporarily using api on local server
+        let urlString = "http://localhost:8000"
+        
+        let url = URL(string: urlString)
+        
+        guard url != nil else {
+            print("url object creation error")
+            return
+        }
+        
+        let session = URLSession.shared
+        
+        let datatask = session.dataTask(with: url!) { (data, response, error) in
+            
+            if error == nil && data != nil {
+                
+                let decoder = JSONDecoder()
+                
+                do {
+                    
+                    let animes = try decoder.decode(AnimeData.self, from: data!)
+                    let animeData = animes.data!
+                    
+                    // Pass data back to view by main thread
+                    DispatchQueue.main.async {
+                        self.delegate?.animeRetrieved(animeData)
+                    }
+                    
+                } catch  {
+                    print("Error parsing json")
+                }
+            }
+            print("Error: Couldn't connect to session")
+        }
+        datatask.resume()
+    }
+}
